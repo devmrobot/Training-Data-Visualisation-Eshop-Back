5578827fe53094fd6a0edc29090ff4fadcbc0679
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-06-28 23:38:19 +0200

    clean express + setup done

---
+++ src/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const connection = require("./db-config");
+const express = require("express");
+const app = express();
+const router = require('./routes/index.routes');
+
+const port = process.env.PORT || 8000;
+
+connection.connect((err) => {
+    if(err) {
+        console.error('error connecting :' + err.stack);
+    } else {
+        console.log("connected as id" + connection.threadId);
+    }
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use("/api", router);
+
+app.get("/", (req,res) => {
+
+    res.send("Welcome on Infatigables's API");
+
+})
+
+app.listen(port, () => {
+    console.log(`Server is listenning on port ${port}`);
+})
+
+module.exports = app;
---
+++ src/routes/index.routes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+const userRouter = require("./users.routes");
+
+router.use('/users', userRouter);
+
+module.exports = router;
