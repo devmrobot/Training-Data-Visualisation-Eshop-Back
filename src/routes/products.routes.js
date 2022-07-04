3b5e58547ed9c914d1c18a087879809087e426f9
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-07-04 12:04:06 +0200

    all select done

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -28,18 +28,57 @@ router.get("/marques-ca", (req, res) => {
 
 module.exports = router;
 
+// CA GLOBAL
 SELECT SUM(p.price), SUM(b.buy_afterclic)
 FROM Produits AS p
 INNER JOIN Buy AS b ON p.idProduits=b.produits_id
 WHERE b.buy_afterclic = 1;
 
+// DUREE CONSULTATION MOYENNE
+SELECT AVG(b.duree_consultation)
+FROM buy AS b;
+
+// NOMBRE DE COMMANDES
+SELECT SUM(b.buy_afterclic)
+FROM buy AS b
+WHERE b.buy_afterclic = 1;
+
+// TX DE CONVERSION COMMANDES ((Nombre total de commandes passées/ Total des visites) * 100)
+SELECT b.buy_afterclic / COUNT(b.date_clic)
+FROM buy AS b
+WHERE b.buy_afterclic = 1;
+
+// TX DE CONVERSION COMMANDES ((Nombre total de commandes passées/ Total des visites) * 100)
+SELECT COUNT(b.buy_afterclic)*100.0/(SELECT COUNT(*) FROM buy)
+FROM buy AS b
+WHERE b.buy_afterclic = 1;
+
+
+// MARQUE & ARTICLES COMMANDEES
 SELECT p.marque, p.price, p.article, b.buy_afterclic
 FROM Produits AS p
 INNER JOIN Buy AS b ON p.idProduits=b.produits_id
 WHERE b.buy_afterclic = 1;
 
+// CA PAR MARQUE
 SELECT p.marque, p.article, SUM(p.price), b.buy_afterclic
 FROM Produits AS p
 INNER JOIN Buy AS b ON p.idProduits=b.produits_id
 WHERE b.buy_afterclic = 1
 GROUP BY p.marque, p.article;
+
+// CA PAR VISITEURS
+SELECT v.nom, v.prénom, b.buy_afterclic, SUM(p.price)
+FROM Visiteurs AS v
+INNER JOIN Buy AS b ON v.idVisiteurs=b.visiteurs_id
+INNER JOIN Produits As p ON b.produits_id=p.idProduits
+WHERE b.buy_afterclic = 1
+GROUP BY v.nom, v.prénom;
+
+// CA & TYPOLOGIE PAR VISITEURS
+SELECT v.nom, v.prénom, b.buy_afterclic, SUM(p.price), p.marque, p.article
+FROM Visiteurs AS v
+INNER JOIN Buy AS b ON v.idVisiteurs=b.visiteurs_id
+INNER JOIN Produits As p ON b.produits_id=p.idProduits
+WHERE b.buy_afterclic = 1
+GROUP BY v.nom, v.prénom, p.marque, p.article;
